b9211bfa78ca6aaef5f439dad34947aa29fadc13
“yoavkaplan123git config --global user.email “yoav110702@gmail.com <yoav110702@gmail.com>
2024-01-22 17:23:24 +0200

    small  fix

--- src/main/java/frc/robot/automations/SorceIntake.java
+++ src/main/java/frc/robot/automations/SorceIntake.java
@@ -34,7 +34,9 @@ public class SorceIntake extends SequentialCommandGroup {
         new WaitUntilCommand(() -> {
           return !Intake.getInstance().isGamePieceInIntake();
         }),
-        new MotorCommand(Intake.getInstance(), -IntakeConstants.intakePower, 0))
+        new MotorCommand(Intake.getInstance(), -IntakeConstants.intakePower, 0)
+      ),
+      new IntakeCommand(IntakeConstants.intakePower)
     );
   }
 }
