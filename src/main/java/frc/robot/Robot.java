b2f0ce108f8646392bb81adc7f35e5d3961ea3f5
GalDotan <gldotan@gmail.com>
2024-02-18 08:01:08 +0200

    Added camera server

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -42,7 +42,7 @@ public class Robot extends TimedRobot {
     LowerShooter.getInstance(), new DefaultRunInternallyControlledSubsystem(
       LowerShooter.getInstance(), 0));
 
-    // CameraServer.startAutomaticCapture();
+    CameraServer.startAutomaticCapture();
   }
 
   @Override
