f989495503cf994474f9778b3018b932bc55ea6f
GalDotan <gldotan@gmail.com>
2024-02-18 08:04:42 +0200

    Removed AngleAdjust Befor Shooting

--- src/main/java/frc/robot/automations/Shoot.java
+++ src/main/java/frc/robot/automations/Shoot.java
@@ -8,8 +8,10 @@ import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.swerve.AngleAdjust;
 import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.shooter.LowerShooter;
@@ -45,7 +47,8 @@ public class Shoot extends SequentialCommandGroup {
     Supplier<Double> elevatorPose = ()  -> ElevatorConstants.SHOOTING_POSE;
     addCommands(
       new ParallelCommandGroup(
-        new AngleAdjust(Shoot::getAngle, () -> 0d, () -> 0d),
+       // new AngleAdjust(Shoot::getAngle, () -> 0d, () -> 0d), ****Currentrl Not In Use****
+       //new WaitCommand(0.1), ****Currentrl Not In Use****
         new GettingReadyToScore(
           UpperShooter.getInstance()::getVelocityForShooting,
           LowerShooter.getInstance()::getVelocityForShooting,
