da5355d8288d46987a68dd26c99416bbbef59f48
GalDotan <gldotan@gmail.com>
2024-03-03 10:13:33 +0200

    Update log class

--- src/main/java/com/ma5951/utils/Logger/MALog.java
+++ src/main/java/com/ma5951/utils/Logger/MALog.java
@@ -1,33 +1,21 @@
 package com.ma5951.utils.Logger;
 
-import java.util.HashMap;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.util.datalog.DataLog;
-import edu.wpi.first.util.datalog.DoubleArrayLogEntry;
-import edu.wpi.first.util.datalog.StringLogEntry;
+
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class MALog {
     private static MALog logger;
-    private final DataLog log;
-
-
+    private final DataLog WPIlogger;
 
     public MALog() {
         DataLogManager.start();
-        log = DataLogManager.getLog();
+        WPIlogger = DataLogManager.getLog();
         DriverStation.startDataLog(DataLogManager.getLog());
     }
 
-    
-
-   
     public static MALog getInstance() {
         if (logger == null) {
             logger = new MALog();
