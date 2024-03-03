2d9143aee9af20811854b0c1b37814bd16507922
GalDotan <gldotan@gmail.com>
2024-03-03 10:55:07 +0200

    Added LoggedSwerveStates

---
+++ src/main/java/com/ma5951/utils/Logger/LoggedSwerveStates.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.ma5951.utils.Logger;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.StructArrayPublisher;
+
+/** Add your docs here. */
+public class LoggedSwerveStates {
+
+    private StructArrayPublisher<SwerveModuleState> loggedStates;
+    private SwerveModuleState[] lastStates;
+
+    public LoggedSwerveStates(NetworkTable networkTable , String name) {
+        loggedStates = networkTable.getStructArrayTopic(name , SwerveModuleState.struct).publish();
+        loggedStates.set(null);
+        lastStates = null;
+    }
+
+    public void update(SwerveModuleState[] states) {
+        if (lastStates != states) {
+            loggedStates.set(states);
+            lastStates = states;
+        }   
+
+    }
+}
